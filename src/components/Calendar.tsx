9716f4df246e04de7dd76b592e22ad42bf8ef3d3
Patricia Cardenas <patricia.cardenas@globant.com>
2022-09-29 23:10:01 -0500

    Styles mobile lanscape

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -20,13 +20,13 @@ export const Calendar = () => {
         <div className='container calendar-container'>
             <h2 className='calendar-title'>NOVIEMBRE 2022</h2>
             <div className='calendar'>
+                <div className='week-day'>Sab</div>
+                <div className='week-day'>Dom</div>
                 <div className='week-day'>Lun</div>
                 <div className='week-day'>Mar</div>
                 <div className='week-day'>Mie</div>
                 <div className='week-day'>Jue</div>
                 <div className='week-day'>Vie</div>
-                <div className='week-day'>Sab</div>
-                <div className='week-day'>Dom</div>
                 {
                     days.map((day) => (
                         <div key={day} className={`day day-${day} ${day == 20 ? ' date' : ''}`}>
