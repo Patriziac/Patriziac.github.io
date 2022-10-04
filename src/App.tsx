89a699dffe229d7ff3dfabb8de56685f080fa3f3
Patricia Cardenas <patricia.cardenas@globant.com>
2022-10-04 18:31:53 -0500

    Redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import './App.css'
 import { DecorationsDiamond } from './components/Decorations/DecorationsDiamond'
 import { DecorationsLeaves } from './components/Decorations/DecorationsLeaves'
@@ -5,11 +6,28 @@ import { InvitationContent } from './components/Invitation/InvitationContent'
 
 function App() {
 
+  const showPage1Handler = () => {
+    let page: any = document.querySelector('.page-1');
+    page.classList.add('fade-out');
+  }
+
+  const showPage2Handler = () => {
+    let page: any = document.querySelector('.page-2');
+    page.classList.add('fade-in');
+  }
+
+  useEffect(() => {
+    //const showPage1 = setTimeout(showPage1Handler, 5000);
+    ///const showPage2 = setTimeout(showPage2Handler, 15000);
+  }, []);
+
   return (
     <div className="App">
-      <DecorationsLeaves />
-      <DecorationsDiamond />
-      <InvitationContent />
+      <div className='page page-1'>
+        <DecorationsLeaves />
+        <DecorationsDiamond />
+        <InvitationContent />
+      </div>
     </div>
   )
 }
