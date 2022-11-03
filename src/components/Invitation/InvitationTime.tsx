b67efbaebe574938fddb76824bb445cc1686708b
Patricia Cardenas <patricia.cardenas@globant.com>
2022-11-03 16:59:24 -0600

    Direccion y hora

--- src/components/Invitation/InvitationTime.tsx
+++ src/components/Invitation/InvitationTime.tsx
@@ -4,7 +4,7 @@ export const InvitationTime = () => {
   return (
     <p className='time'>
       <span>
-        A la <span className='hour'>1:00</span> pm
+        A la <span className='hour'>12:30</span> pm
       </span>
     </p>
   )
